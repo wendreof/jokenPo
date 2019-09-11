594280041ec64b63518f9487e6a0b122f01192a7
Wendreo <wendreolf@gmail.com>
2019-09-10 21:46:03 -0300

    :rocket:

---
+++ lib/Game.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('JokenPo')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text('Escolha do App',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Image.asset("images/padrao.png"),
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text('Escolha uma das opções',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(
+                "images/pedra.png",
+                height: 100,
+              ),
+              Image.asset(
+                "images/papel.png",
+                height: 100,
+              ),
+              Image.asset(
+                "images/tesoura.png",
+                height: 100,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
